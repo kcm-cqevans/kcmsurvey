c86cc27105291c97bd005369133edca07c139f4b
kcm-cqevans <clevans@kingcounty.gov>
2024-04-23 16:07:16 -0700

    adding fonts

---
+++ R/kcmviz_fonts.R
@@ -0,0 +1,13 @@
+#' Add Google fonts for visualization
+#'
+#' This function adds Google fonts for visualization purposes using sysfonts and showtext.
+#'
+#' @keywords internal
+#' @export
+kcmviz_fonts <- function() {
+  sysfonts::font_add_google("lato", "lato")
+  sysfonts::font_add_google("inter", "inter")
+  sysfonts::font_add_google("inter tight", "inter tight")
+  sysfonts::font_add_google("inter", family = "inter-light", regular.wt = 300)
+  showtext::showtext_auto()
+}
