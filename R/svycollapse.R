c77acb83ae37dabf46a0f077e62cf1a44ebe8d0b
kcm-cqevans <clevans@kingcounty.gov>
2025-01-08 17:52:25 -0800

    updating svy collapse

--- R/svycollapse.R
+++ R/svycollapse.R
@@ -16,12 +16,12 @@
 #' @export
 #'
 
-svycollapse <- function(data, element_var = NULL, response_category = NULL, wgt = NULL, groupby_var = NULL,
-                         data_shape=NULL, dv_variabletype = NULL) {
+svycollapse <- function(data, element_var = NULL, response_category = NULL, wgt = NULL,
+                        groupby_var = NULL, binary_response_category=FALSE) {
   # Check if columns exist in data, if not, use the provided parameters
-  if (!all(c("response_category", "wgt") %in% names(data))) {
-    if(is.null(response_category) || is.null(wgt)) {
-      stop("missing response_category or wgt columns")
+  if (!all(c("element_var", "response_category", "wgt") %in% names(data))) {
+    if(is.null(element_var) || is.null(response_category) || is.null(wgt)) {
+      stop("missing element_var, response_category or wgt columns")
     }
   } else {
     element_var <- "element_var"
@@ -29,94 +29,42 @@ svycollapse <- function(data, element_var = NULL, response_category = NULL, wgt 
     wgt <- "wgt"
   }
 
-  if(data_shape=="long" & !is.null(groupby_var)) {
+  # Adjusted to directly use column names rather than data$column
+  if (!is.null(groupby_var) & binary_response_category==TRUE) {
     data %>%
       filter(!is.na(.data[[groupby_var]]), !is.na(.data[[element_var]]), !is.na(.data[[response_category]])) %>%
       as_survey_design(ids = 1, weights = .data[[wgt]]) %>%
       group_by(.data[[groupby_var]], .data[[element_var]], .data[[response_category]]) %>%
-      summarize(prop = 100 * survey_mean(variable = .data[[response_category]], na.rm = TRUE, vartype="ci"), .groups = 'drop') %>%
-      #filter(.data[[response_category]] == 100 | .data[[response_category]] == 1) %>%
-      mutate(proplabel = paste0(round(prop, 1), "%")) %>%
-      dplyr::rename(groupby_var:=!!groupby_var)
-  }
-
-  if(data_shape=="long" & !is.null(groupby_var) & dv_variabletype=="binary") {
+      summarize(prop = 100 * survey_mean(variable = .data[[response_category]], na.rm = TRUE), .groups = 'drop') %>%
+      filter(.data[[response_category]] == 100 | .data[[response_category]] == 1) %>%
+      mutate(proplabel = paste0(round(prop, 1), "%"))
+  } else if (!is.null(groupby_var) & binary_response_category==FALSE) {
     data %>%
       filter(!is.na(.data[[groupby_var]]), !is.na(.data[[element_var]]), !is.na(.data[[response_category]])) %>%
       as_survey_design(ids = 1, weights = .data[[wgt]]) %>%
       group_by(.data[[groupby_var]], .data[[element_var]], .data[[response_category]]) %>%
-      summarize(prop = 100 * survey_mean(variable = .data[[response_category]], na.rm = TRUE, vartype="ci"), .groups = 'drop') %>%
-      filter(.data[[response_category]] == 100 | .data[[response_category]] == 1) %>%
-      mutate(proplabel = paste0(round(prop, 1), "%")) %>%
-      dplyr::rename(groupby_var:=!!groupby_var)
+      summarize(prop = 100 * survey_mean(variable = .data[[response_category]], na.rm = TRUE), .groups = 'drop') %>%
+      # filter(.data[[response_category]] == 100 | .data[[response_category]] == 1) %>%
+      mutate(proplabel = paste0(round(prop, 1), "%"))
   }
-
-  else if(data_shape=="long" & is.null(groupby_var)) {
+  else if (is.null(groupby_var) & binary_response_category==FALSE) {
     data %>%
       filter(!is.na(.data[[element_var]]), !is.na(.data[[response_category]])) %>%
       as_survey_design(ids = 1, weights = .data[[wgt]]) %>%
       group_by(.data[[element_var]], .data[[response_category]]) %>%
-      summarize(prop = 100 * survey_mean(variable = .data[[response_category]], na.rm = TRUE, vartype="ci"), .groups = 'drop') %>%
-      #filter(.data[[response_category]] == 100 | .data[[response_category]] == 1) %>%
+      summarize(prop = 100 * survey_mean(variable = .data[[response_category]], na.rm = TRUE), .groups = 'drop') %>%
+      # filter(.data[[response_category]] == 100 | .data[[response_category]] == 1) %>%
       mutate(proplabel = paste0(round(prop, 1), "%"))
   }
 
-  else if(data_shape=="long" & is.null(groupby_var) & dv_variabletype=="binary") {
+  else if (is.null(groupby_var) & binary_response_category==TRUE) {
     data %>%
       filter(!is.na(.data[[element_var]]), !is.na(.data[[response_category]])) %>%
       as_survey_design(ids = 1, weights = .data[[wgt]]) %>%
       group_by(.data[[element_var]], .data[[response_category]]) %>%
-      summarize(prop = 100 * survey_mean(variable = .data[[response_category]], na.rm = TRUE, vartype="ci"), .groups = 'drop') %>%
+      summarize(prop = 100 * survey_mean(variable = .data[[response_category]], na.rm = TRUE), .groups = 'drop') %>%
       filter(.data[[response_category]] == 100 | .data[[response_category]] == 1) %>%
       mutate(proplabel = paste0(round(prop, 1), "%"))
   }
 
-
-  else if(data_shape=="wide" & !is.null(groupby_var)) {
-    data %>%
-      filter(!is.na(.data[[groupby_var]]), !is.na(.data[[response_category]])) %>%
-      as_survey_design(ids = 1, weights = .data[[wgt]]) %>%
-      group_by(.data[[groupby_var]], .data[[response_category]]) %>%
-      summarize(prop = 100 * survey_mean(variable = .data[[response_category]], na.rm = TRUE, vartype="ci"), .groups = 'drop') %>%
-      #filter(.data[[response_category]] == 100 | .data[[response_category]] == 1) %>%
-      mutate(proplabel = paste0(round(prop, 1), "%")) %>%
-      dplyr::rename(element_var:=!!response_category,
-                    groupby_var:=!!groupby_var)
-  }
-
-  else if(data_shape=="wide" & !is.null(groupby_var) & dv_variabletype=="binary") {
-    data %>%
-      filter(!is.na(.data[[groupby_var]]), !is.na(.data[[response_category]])) %>%
-      as_survey_design(ids = 1, weights = .data[[wgt]]) %>%
-      group_by(.data[[groupby_var]], .data[[response_category]]) %>%
-      summarize(prop = 100 * survey_mean(variable = .data[[response_category]], na.rm = TRUE, vartype="ci"), .groups = 'drop') %>%
-      filter(.data[[response_category]] == 100 | .data[[response_category]] == 1) %>%
-      mutate(proplabel = paste0(round(prop, 1), "%")) %>%
-      dplyr::rename(element_var:=!!response_category,
-                    groupby_var:=!!groupby_var)
-  }
-  else if(data_shape=="wide" & is.null(groupby_var)) {
-    data %>%
-      filter(!is.na(.data[[response_category]])) %>%
-      as_survey_design(ids = 1, weights = .data[[wgt]]) %>%
-      group_by(.data[[response_category]]) %>%
-      summarize(prop = 100 * survey_mean(variable = .data[[response_category]], na.rm = TRUE, vartype="ci"), .groups = 'drop') %>%
-      #filter(.data[[response_category]] == 100 | .data[[response_category]] == 1) %>%
-      mutate(proplabel = paste0(round(prop, 1), "%")) %>%
-      dplyr::rename(element_var:=!!response_category)
-
-  }
-
-  else if(data_shape=="wide" & is.null(groupby_var) & dv_variabletype=="binary") {
-    data %>%
-      filter(!is.na(.data[[response_category]])) %>%
-      as_survey_design(ids = 1, weights = .data[[wgt]]) %>%
-      group_by(.data[[response_category]]) %>%
-      summarize(prop = 100 * survey_mean(variable = .data[[response_category]], na.rm = TRUE, vartype="ci"), .groups = 'drop') %>%
-      filter(.data[[response_category]] == 100 | .data[[response_category]] == 1) %>%
-      mutate(proplabel = paste0(round(prop, 1), "%")) %>%
-      dplyr::rename(element_var:=!!response_category)
-
-  }
 }
-
